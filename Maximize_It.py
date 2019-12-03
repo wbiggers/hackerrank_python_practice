49a17155a6518eef0db6734f220e264e352d784e
wbiggers <wes.biggers@gmail.com>
2019-12-03 16:26:37 -0500

    Successfully Submitted

--- Maximize_It.py
+++ Maximize_It.py
@@ -18,20 +18,25 @@ Output Format
 Output a single integer denoting the value Smax.
 """
 
-# Logic - use itertools combinations to create all possible subsets. Calculate S for each.  Print max S.
-# Modulo does have a distributive property, though it does not eliminate the need to consolidate the entire numerator
-# and apply the denominator in a final modulo operation.  Therefore, we will apply the f(x) operation upon input()
-# but not the distributive modulo as it adds an additional operation rather than simplify the process.
+# Logic - first - only calculate the square of each number once - when building the lists.
+# Second - use itertools product to create all possible subsets.
+# Third - use list comprehension to create a single list of the value of S for each combination
+# Print max S.
+
+from itertools import product
+
+
+def square(num):
+    return num**2
+
 
 # Handle the input:
 K, M = map(int, input().split())
 N = []
 for i in range(K):
-    N.append(list(map(int, input().split())))
-    print(N)
+    N.append(list(map(square, map(int, input().split()))))
     N[i].pop(0)
-    print(N)
 
-
-
-
+all_combos = product(*N)
+S = list(sum(item) % M for item in all_combos)
+print(max(S))
